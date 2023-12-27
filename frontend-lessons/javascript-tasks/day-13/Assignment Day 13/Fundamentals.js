f1a1fafcb6e2ce411febacc546eae62acf1b231e
Renzo Osmillo <osmillo08@gmail.com>
2023-12-27 12:24:36 +0800

    JS Assignment Day 13
    Finished the JavaScript task for day 13.

---
+++ frontend-lessons/javascript-tasks/day-13/Assignment Day 13/Fundamentals.js
@@ -0,0 +1,32 @@
+// Task 1: Variable Mastery
+
+let currentLesson = "JavaScript Fundamentals";
+console.log(currentLesson);
+
+// Task 2: Data Types Exploration
+
+let favoriteNumber = 8;
+let favoriteWord = "weird";
+let isJavaScriptFun = true;
+let nothing = null;
+let undefinedVar;
+
+// Task 3: Logical Operators in Action
+
+let isMorning = true;
+let isRaining = true;
+
+if (isMorning && isRaining) {
+    console.log("Let's find rainbow!")
+} else {
+    console.log("Stay and chill at home.")
+}
+
+if (isMorning || isRaining) {
+    console.log("Let's drink a coffee!");
+} else {
+    console.log("Take the day off.")
+}
+
+isMorning = !isMorning;
+console.log(isMorning)
